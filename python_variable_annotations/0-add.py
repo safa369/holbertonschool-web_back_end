7badb5cec67497cc81e3c26a3ed17126cf64f712
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-05 13:51:59 +0100

    add function

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 
 def add(a: float, b: float) -> float:
     """functio return the sum off two float
-    a: float
-    b: float"""
+            a: float
+            b: float
+        return: float"""
     return a + b
 
