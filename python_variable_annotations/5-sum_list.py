47638cb3787326b0cdb6dbc5c84ac4d49ac01abf
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-08 11:50:36 +0100

    sum of list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """model for sum of list"""
+import typing
 
 
-def sum_list(input_list: [float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """function that calcul the sum of list"""
     sum = 0
     for i in input_list:
