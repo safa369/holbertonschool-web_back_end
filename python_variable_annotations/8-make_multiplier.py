a87e50b0cb88a7327cba82948ffc4532658d5a38
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-08 15:04:51 +0100

    make multiplier

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,7 @@ import typing
 
 
 def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+    """function take a float as arg and return a function"""
     def make_multiplier_m(x: float) -> float:
         return (multiplier * x)
     return make_multiplier_m
