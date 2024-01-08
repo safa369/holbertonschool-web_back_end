399b06e94884d2c16772c11c4d15b86660b70e7a
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-08 11:16:21 +0100

    sum of list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""model for sum of list"""
+
+
+def sum_list(input_list: [float]) -> float:
+    """function that calcul the sum of list"""
+    sum = 0
+    for i in input_list:
+        sum += i
+    return sum
