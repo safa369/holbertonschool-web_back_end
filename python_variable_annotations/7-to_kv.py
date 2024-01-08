49a04d7eda426d41e9b5a8393b813ac770a516a8
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-08 14:07:35 +0100

    tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,7 +5,5 @@ import typing
 
 def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
     """function that return a tuple"""
-    if type(v) is int:
-        v = float(v)
-    tupl = (k, v)
+    tupl = (k, v*v)
     return tupl
