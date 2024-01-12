6228a5ade403fee2e21510c8165593fc8302133f
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-12 19:31:37 +0100

    coroutine

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
+"""model execute multiple coroutines"""
 import asyncio
 import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """function that return a list of n float """
     liss = []
-    x = int(await wait_random(n))
-    for _ in range(x):
+
+    for _ in range(n):
         lis = await wait_random(max_delay)
         liss.append(float(lis))
-    for j in range(x):
+    for j in range(n):
         min_x = j
-        for y in range(min_x + 1, x):
+        for y in range(min_x + 1, n):
             if liss[min_x] > liss[y]:
                 liss[min_x], liss[y] = liss[y], liss[min_x]
     return liss
