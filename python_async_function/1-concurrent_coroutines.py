eb231185713f1fb41d0a07378fdb4d12822d29f3
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-12 17:48:49 +0100

    coroutine

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import asyncio
+import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> "list[float]":
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     liss = []
     x = int(await wait_random(n))
     for _ in range(x):
