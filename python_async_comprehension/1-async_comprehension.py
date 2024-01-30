3078948456a10230ac4a71c363b8d220921d4859
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-30 15:56:46 +0100

    asynchrone

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,4 +5,5 @@ async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """collect async generator list and return it"""
     return [_ async for _ in async_generator()]
