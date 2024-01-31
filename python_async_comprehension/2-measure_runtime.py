f64ce020d610d8cd009c0768d701a63e8610af78
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-31 12:47:18 +0100

    measure runtime

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,6 +8,6 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 async def measure_runtime() -> float:
     t1 = time.time()
     awaitable = [async_comprehension() for _ in range(4)]
-    result = await asyncio.gather(*awaitable, asyncio.sleep(10))
+    result = await asyncio.gather(*awaitable)
     t2 = time.time()
     print(t2 - t1)
