92f6e2d384a5ebc163c6630c70d4d8de64e9fa33
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-01 13:59:36 +0100

    insert

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -7,8 +7,8 @@ import pymongo
 
 def insert_school(mongo_collection, **kwargs):
     """function return _id """
-    new_colection = {}
+    new_collection = {}
     for key, value in kwargs.items():
-        new_colection['key'] = 'value'
-    result = mongo_collection.insert_one(new_colection)
-    return result.inserted_id
+        new_collection['key'] = 'value'
+    result = mongo_collection.insert_one(new_collection)
+    return new_collection.inserted_id
