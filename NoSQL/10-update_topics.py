38c7484dce5c415d72cfc58baa574ccb7dde1363
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-01 15:00:27 +0100

    upodate

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,6 +5,7 @@ import pymongo
 
 
 def update_topics(mongo_collection, name, topics):
+    """update topics"""
     myquery = {"name": name}
     new_values = {"$set": {"topics": topics}}
     mongo_collection.update(myquery, new_values)
