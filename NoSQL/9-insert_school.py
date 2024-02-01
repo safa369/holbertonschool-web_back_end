6ea8423ae9d78ca0b2460c076bf6d851b713e3d0
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-01 13:56:12 +0100

    insert

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""function insert a new document"""
+
+from bson.objectid import ObjectId
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    new_colection = {}
+    for key, value in kwargs.items():
+        new_colection['key'] = 'value'
+    result = mongo_collection.insert_one(new_colection)
+    return result.inserted_id
