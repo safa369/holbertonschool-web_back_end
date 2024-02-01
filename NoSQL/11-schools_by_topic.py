6cba1093209b36f122ddca60d5c9169caf0191d8
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-01 15:23:06 +0100

    liste

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """list of school"""
 
-import pymongo
-
 
 def school_by_topic(mongo_collection, topic):
     """function return a list of document"""
-    return mongo_collection.find({"topic": topic})
+    return mongo_collection.find_one({"topics": topic})
