97905a2ccf012d65173828bb9adbe5e86726205c
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-02 16:20:29 +0100

    simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -4,6 +4,13 @@ import math
 from typing import List
 
 
+def index_range(page, page_size):
+    """function that return a tuple"""
+    start = (page - 1) - page_size
+    end = page * page_size
+    return start, end
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -19,22 +26,19 @@ class Server:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
+                count = sum(1 for row in reader)
             self.__dataset = dataset[1:]
-
         return self.__dataset
 
-    def index_range(page, page_size):
-        """function that return a tuple"""
-        start = (page - 1) - page_size
-        end = page * page_size
-        return start, end
-
-    def get_page(self, page: int = 1,
-                 page_size: int = 10) -> List[List]:
-        if (not isinstance(page, int)
-            or not isinstance(page_size, int)
-            or page <= 0
-            or page_size <= 0):
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        if (not isinstance(page, int) or not isinstance(page_size, int) or
+            page <= 0 or page_size <= 0):
             raise AssertionError
-        start, end = Server.index_range(page, page_size)
-        return self.dataset()[start:end]
+        with open(self.DATA_FILE, 'r') as f:
+            reader = csv.reader(f)
+            count = sum(1 for row in reader)
+        start, end = index_range(page, page_size)
+        if count < end:
+            return []
+        else:
+            return self.dataset()[start:end]
