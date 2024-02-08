e818c9f5e6233916905a52f85798a4c44502ded8
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-08 11:39:25 +0100

    block scoped

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,9 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse){
-    const task = true;
-    const task2 = false;
-    
+    const task = false;
+    const task2 = true;
+
     return [task, task2];
   }
   return [task, task2];
