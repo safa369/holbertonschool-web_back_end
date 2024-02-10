8a77ac07d9958d05a19d5c11f76700943d8e5638
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-10 15:58:04 +0100

    course

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,5 @@
 export default class HolbertonCourse {
-  constructor(name = "", length = 0, students = []) {
+  constructor(name = '', length = 0, students = []) {
     this.name = name;
     this.length = length;
     this.students = students;
