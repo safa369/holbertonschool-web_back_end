4cfca959054164199eddfcb70a15e17c3c71a532
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-11 18:04:47 +0100

    sky

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,30 @@
+import Building from "./5-building";
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.sqft = sqft;
+    this.floors = floors;
+  }
+
+  get gsqft() {
+    return this._sqft;
+  }
+
+  set gsqft(s) {
+    this._sqft = s
+  }
+  
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(val) {
+    this._floors = val;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+    }
+
+}
