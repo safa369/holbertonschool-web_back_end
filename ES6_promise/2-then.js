7b7745b656c257ee1b81b8440c73ac394f207539
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-14 16:45:19 +0100

    then

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -3,16 +3,15 @@ export default function handleResponseFromAPI(promise) {
     status: 200,
     body: 'Success',
   };
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     if (promise) {
       resolve(obj);
+    } else {
+        reject()
     }
   })
     .then((response) => {
       console.log('Got a response from the API');
       return response;
-    })
-    .catch((err) => {
-      console.log(err);
     });
 }
