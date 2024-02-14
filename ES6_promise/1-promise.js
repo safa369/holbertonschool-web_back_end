ed18b6360672509717ab903d1d917c3a48b30e14
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-14 15:23:49 +0100

    promise

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getResponseFromAPI(success) {
+  const obj = {
+    status: 200,
+    body: 'Success',
+  };
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve(obj);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
