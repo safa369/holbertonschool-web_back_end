411aadaace2835141ff6712cbbc4031396d0391d
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-15 13:22:43 +0100

    commit

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,9 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    let UP = signUpUser(firstName, lastName);
-    let PR = uploadPhoto(fileName);
-    Promise.allSettled([UP, PR])
-    .then(values => console.log(values))
-    .catch(error => console.error(error));
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+  Promise.allSettled([user, photo])
+    .then((values) => console.log(values));
 }
