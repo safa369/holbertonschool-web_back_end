62182dfe100dd6d2a7eceb32c0cb1af484d102ec
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-15 14:18:57 +0100

    commit

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,6 +4,16 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
-  Promise.allSettled([user, photo])
-    .then((values) => console.log(values));
+  return Promise.allSettled([user, photo])
+    .then((values) => {
+      const result = [];
+      values.forEach((values) => {
+        if (values.status === 'fulfilled') {
+          result.push({ status: values.status, value: values.value });
+        } else {
+          result.push({ status: values.status, reason: values.reason });
+        }
+      });
+      return result;
+    });
 }
