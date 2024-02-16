f35d691123a7a94d1bea8d06aaed3a9d10130cb1
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-16 11:53:22 +0100

    try catch

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -2,9 +2,8 @@ export default function guardrail(mathFunction) {
   const queue = [];
   try {
     const result = mathFunction();
-      queue.push(result);
-    }
-   catch (e) {
+    queue.push(result);
+  } catch (e) {
     queue.push(`${e}`);
   } finally {
     queue.push('Guardrail was processed');
