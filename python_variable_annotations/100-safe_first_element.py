7cf2c5b28e4fe9807637470855f49c583421c8f4
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-03-30 00:26:15 +0100

    optional

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python3
+"""safe first element"""
+from typing import Any, Optional, Sequence
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    if lst:
+        return lst[0]
+    else:
+        return None
