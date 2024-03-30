a446c9fb037eadb39da7805f90f847d427ef4e1c
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-03-30 23:51:58 +0100

    hypermedia del pagination

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -9,8 +9,7 @@ from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -18,8 +17,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -29,8 +27,7 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict:
-        """Dataset indexed by sorting position, starting at 0
-        """
+        """Dataset indexed by sorting position, starting at 0"""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
@@ -41,6 +38,7 @@ class Server:
 
     def get_hyper_index(self, index: int = None,
                         page_size: int = 10) -> Dict[int, List]:
+        """hyper index"""
         dataset = self.dataset()
         total_pages = math.ceil(len(dataset) / page_size)
 
@@ -60,8 +58,7 @@ class Server:
 
         return {
             "index": start_index,
-            "next_index": next_index,
-            "page_size": page_size,
             "data": data,
-            "total_pages": total_pages,
+            "page_size": page_size,
+            "next_index": next_index,
         }
