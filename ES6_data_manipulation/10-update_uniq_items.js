d5d530a971cb3bba70970b4396caf15fd91700a5
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-01 22:53:02 +0200

    map

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,13 +2,9 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  const updatedMap = new Map();
   map.forEach((value, key) => {
     if (value === 1) {
-      updatedMap.set(key, 100);
-    } else {
-        updatedMap.set(key, value);
+      map.set(key, 100);
     }
   });
-  return updatedMap;
 }
